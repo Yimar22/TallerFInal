13bdba6ccef77e076d7fbde5f1f1c5719d6ef331
Yimar <yimar2001@gmail.com>
2021-12-14 00:35:08 -0500

    Just package front.model

--- src/main/java/com/tamayo/front/controller/implementation/ProductControllerImp.java
+++ src/main/java/com/tamayo/front/controller/implementation/ProductControllerImp.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.tamayo.back.model.Product;
@@ -21,6 +22,7 @@ import com.tamayo.front.controller.interfaces.ProductController;
 
 
 @Controller
+@RequestMapping("/front/Product")
 public class ProductControllerImp implements ProductController{
 	
 	private BusinessDelegate businessDelegate;
--- src/main/java/com/tamayo/front/controller/interfaces/UserLoginController.java
+++ src/main/java/com/tamayo/front/controller/interfaces/UserLoginController.java
@@ -4,7 +4,5 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.ui.Model;
 
 public interface UserLoginController {
-	public String customLogin(Model model);
-	public String index();
-	public String accessDeniedPage(@Param(value = "url") String url, Model model);
+	public String login();
 }
