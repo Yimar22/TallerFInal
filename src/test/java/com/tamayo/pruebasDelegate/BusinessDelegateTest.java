54d64186d1f31416fb7beece0617477de5ca5d51
Yimar <yimar2001@gmail.com>
2021-12-17 12:40:31 -0500

    Test for business delegate done

--- src/test/java/com/tamayo/pruebasDelegate/BusinessDelegateTest.java
+++ src/test/java/com/tamayo/pruebasDelegate/BusinessDelegateTest.java
@@ -139,7 +139,30 @@ public class BusinessDelegateTest {
 					assertTrue(specialOfferProduct.equals(specialOfferProduct2));
 				}
 
-	
+		//SalesOrderDetail
+			public void saveSalesOrderDetailTest() {
+				Salesorderdetail salesOrderDetail = new Salesorderdetail();
+				salesOrderDetail.setId(52);
+				Date date = new Date();
+				Timestamp timestamp = new Timestamp(date.getTime());
+				salesOrderDetail.setModifieddate(timestamp);
+				BigDecimal price = new BigDecimal(100);
+				salesOrderDetail.setUnitprice(price);
+				
+				Mockito.when(restTemplate.postForEntity(SOD_URL, salesOrderDetail, Salesorderdetail.class))
+				.thenReturn(new ResponseEntity<Salesorderdetail>(salesOrderDetail, HttpStatus.OK));
+				Mockito.when(restTemplate.getForObject(SOD_URL + salesOrderDetail.getId(), Salesorderdetail.class)).thenReturn(salesOrderDetail);
+
+				businessDelegate.salesOrderDetailSave(salesOrderDetail);
+				assertEquals(businessDelegate.salesOrderDetailFindById(52), salesOrderDetail.getId(),
+						"The ID should be 52");
+				
+				
+				Salesorderdetail salesOrderDetail2 = businessDelegate.salesOrderDetailFindById(salesOrderDetail.getId());
+				
+				assertTrue(salesOrderDetail.equals(salesOrderDetail2));
+
+			}
 	}
 
 	@Nested
@@ -247,7 +270,38 @@ public class BusinessDelegateTest {
 					assertEquals(specialOfferProduct2.getId(), specialOfferProductTest.getId());
 					assertEquals(specialOfferProduct2.getRowguid(), specialOfferProductTest.getRowguid());
 				}
-		
+				
+
+		//SalesOrderDetail
+			public void editSalesOrderDetailTest() {
+				Salesorderdetail salesOrderDetail = new Salesorderdetail();
+				salesOrderDetail.setId(52);
+				Date date = new Date();
+				Timestamp timestamp = new Timestamp(date.getTime());
+				salesOrderDetail.setModifieddate(timestamp);
+				BigDecimal price = new BigDecimal(100);
+				salesOrderDetail.setUnitprice(price);
+				
+				Salesorderdetail salesOrderDetail2 = new Salesorderdetail();
+				salesOrderDetail.setId(69);
+				Date date2 = new Date();
+				Timestamp timestamp2 = new Timestamp(date2.getTime());
+				salesOrderDetail.setModifieddate(timestamp2);
+				BigDecimal price2 = new BigDecimal(180);
+				salesOrderDetail.setUnitprice(price2);
+				
+				Mockito.when(restTemplate.postForEntity(SOD_URL, salesOrderDetail, Salesorderdetail.class))
+				.thenReturn(new ResponseEntity<Salesorderdetail>(salesOrderDetail, HttpStatus.OK));
+				Mockito.doNothing().when(restTemplate).put(SOD_URL + salesOrderDetail.getId(), salesOrderDetail2, Salesorderdetail.class);
+				Mockito.when(restTemplate.getForObject(SOD_URL + salesOrderDetail.getId(), Salesorderdetail.class)).thenReturn(salesOrderDetail2);
+				
+				businessDelegate.salesOrderDetailSave(salesOrderDetail);
+				businessDelegate.salesOrderDetailEdit(salesOrderDetail2);
+
+				Salesorderdetail salesOrderDetailTest = businessDelegate.salesOrderDetailFindById(salesOrderDetail.getId());
+				assertEquals(salesOrderDetail2.getId(), salesOrderDetailTest.getId());
+				assertEquals(salesOrderDetail.getUnitprice(), salesOrderDetailTest.getUnitprice());
+			}
 	}
 
 	@Nested
@@ -333,6 +387,30 @@ public class BusinessDelegateTest {
 			assertNotNull(businessDelegate.specialofferproductFindAll(), "The list should not be null");
 		}
 
+		//SalesOrderDetail
+		public void findAllSalesOrderDetailTest() {
+			Salesorderdetail salesOrderDetail = new Salesorderdetail();
+			salesOrderDetail.setId(52);
+			Date date = new Date();
+			Timestamp timestamp = new Timestamp(date.getTime());
+			salesOrderDetail.setModifieddate(timestamp);
+			BigDecimal price = new BigDecimal(100);
+			salesOrderDetail.setUnitprice(price);
+			
+			Salesorderdetail salesOrderDetail2 = new Salesorderdetail();
+			salesOrderDetail.setId(69);
+			Date date2 = new Date();
+			Timestamp timestamp2 = new Timestamp(date2.getTime());
+			salesOrderDetail.setModifieddate(timestamp2);
+			BigDecimal price2 = new BigDecimal(180);
+			salesOrderDetail.setUnitprice(price2);
+			
+			ArrayList<Salesorderdetail> list = new ArrayList<Salesorderdetail>();
+			list.add(salesOrderDetail);
+			list.add(salesOrderDetail2);
+			when(businessDelegate.salesOrderDetailFindAll()).thenReturn(list);
+			assertNotNull(businessDelegate.salesOrderDetailFindAll(), "The list should not be null");
+		}
 	}
 
 	@Nested
@@ -353,14 +431,11 @@ public class BusinessDelegateTest {
 			
 			Mockito.when(restTemplate.postForEntity(PROD_URL, product, Product.class))
 			.thenReturn(new ResponseEntity<Product>(product, HttpStatus.OK));
-			businessDelegate.productSave(product);
 			
-			Mockito.doNothing().when(restTemplate).put(PROD_URL + product.getProductid(), product, Product.class);
-			businessDelegate.productDelete(product.getProductid());
 
 			Mockito.when(restTemplate.getForObject(PROD_URL + product.getProductid(), Product.class)).thenReturn(product);
 			
-		
+			businessDelegate.productSave(product);
 			Product Producttest = businessDelegate.productFindById(product.getProductid());
 			
 			assertTrue(Producttest.getProductnumber().equals(product.getProductnumber()));
@@ -379,14 +454,11 @@ public class BusinessDelegateTest {
 		
 		Mockito.when(restTemplate.postForEntity(SO_URL, specialOffer, Specialoffer.class))
 		.thenReturn(new ResponseEntity<Specialoffer>(specialOffer, HttpStatus.OK));
-		businessDelegate.specialofferSave(specialOffer);
 		
-		Mockito.doNothing().when(restTemplate).put(SO_URL + specialOffer.getSpecialofferid(), specialOffer, Specialoffer.class);
-		businessDelegate.productDelete(specialOffer.getSpecialofferid());
 
 		Mockito.when(restTemplate.getForObject(SO_URL + specialOffer.getSpecialofferid(), Specialoffer.class)).thenReturn(specialOffer);
 		
-	
+		businessDelegate.specialofferSave(specialOffer);
 		Specialoffer specialOffertest = businessDelegate.specialofferFindById(specialOffer.getSpecialofferid());
 		
 		assertTrue(specialOffertest.getSpecialofferid().equals(specialOffer.getSpecialofferid()));
@@ -407,19 +479,40 @@ public class BusinessDelegateTest {
 			
 			Mockito.when(restTemplate.postForEntity(SOP_URL, specialOfferProduct, Specialofferproduct.class))
 			.thenReturn(new ResponseEntity<Specialofferproduct>(specialOfferProduct, HttpStatus.OK));
-			businessDelegate.specialofferproductSave(specialOfferProduct);
 			
-			Mockito.doNothing().when(restTemplate).put(SOP_URL + specialOfferProduct.getId(), specialOfferProduct, Specialofferproduct.class);
-			businessDelegate.specialofferproductDelete(specialOfferProduct);
 
 			Mockito.when(restTemplate.getForObject(SOP_URL + specialOfferProduct.getId(), Specialofferproduct.class)).thenReturn(specialOfferProduct);
 			
-		
+			businessDelegate.specialofferproductSave(specialOfferProduct);
+
 			Specialofferproduct specialOfferProducttest = businessDelegate.specialofferproductFindById(specialOfferProduct.getId());
 			
 			assertTrue(specialOfferProducttest.getId().equals(specialOfferProduct.getId()));
 
 		}
+		
+		//SalesOrderDetail
+				public void findByIdSalesOrderDetailTest() {
+					Salesorderdetail salesOrderDetail = new Salesorderdetail();
+					salesOrderDetail.setId(52);
+					Date date = new Date();
+					Timestamp timestamp = new Timestamp(date.getTime());
+					salesOrderDetail.setModifieddate(timestamp);
+					BigDecimal price = new BigDecimal(100);
+					salesOrderDetail.setUnitprice(price);
+					
+					Mockito.when(restTemplate.postForEntity(SOD_URL, salesOrderDetail, Salesorderdetail.class))
+					.thenReturn(new ResponseEntity<Salesorderdetail>(salesOrderDetail, HttpStatus.OK));
+					
+
+					Mockito.when(restTemplate.getForObject(SOD_URL + salesOrderDetail.getId(), Salesorderdetail.class)).thenReturn(salesOrderDetail);
+					
+					businessDelegate.salesOrderDetailSave(salesOrderDetail);
+
+					Salesorderdetail specialOfferProducttest = businessDelegate.salesOrderDetailFindById(salesOrderDetail.getId());
+					
+					assertTrue(specialOfferProducttest.getId().equals(salesOrderDetail.getId()));
+			}
 		
 	}
 
@@ -444,7 +537,7 @@ public class BusinessDelegateTest {
 			businessDelegate.productSave(product);
 
 			Mockito.doNothing().when(restTemplate).put(PROD_URL + product.getProductid(), product, Product.class);
-			businessDelegate.productDelete(product.getProductid());
+			businessDelegate.productDelete(product);
 
 			when(restTemplate.getForObject(PROD_URL + product.getProductid(), null))
 			.thenReturn(new ResponseEntity(null, HttpStatus.OK).getBody());
@@ -505,6 +598,32 @@ public class BusinessDelegateTest {
 			assertNull(businessDelegate.specialofferproductFindById(specialOfferProduct.getId()));
 			
 		}
-	}	
+		
+		//SalesOrderDetail
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		public void deleteSalesOrderDetailTest() {
+			Salesorderdetail salesOrderDetail = new Salesorderdetail();
+			salesOrderDetail.setId(52);
+			Date date = new Date();
+			Timestamp timestamp = new Timestamp(date.getTime());
+			salesOrderDetail.setModifieddate(timestamp);
+			BigDecimal price = new BigDecimal(100);
+			salesOrderDetail.setUnitprice(price);
+			
+
+			Mockito.when(restTemplate.postForEntity(SOD_URL, salesOrderDetail, Salesorderdetail.class))
+			.thenReturn(new ResponseEntity<Salesorderdetail>(salesOrderDetail, HttpStatus.OK));
+			businessDelegate.salesOrderDetailSave(salesOrderDetail);
+
+			Mockito.doNothing().when(restTemplate).put(SOD_URL + salesOrderDetail.getId(), salesOrderDetail, Salesorderdetail.class);
+			businessDelegate.salesOrderDetailDelete(salesOrderDetail);
+
+			when(restTemplate.getForObject(SOD_URL + salesOrderDetail.getId(), null))
+			.thenReturn(new ResponseEntity(null, HttpStatus.OK).getBody());
+			
+			assertNull(businessDelegate.salesOrderDetailFindById(salesOrderDetail.getId()));
+			
+		}	
 	
+	}
 }
